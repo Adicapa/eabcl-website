88c0c52b8a9beba8aaef3405c993e49707609206
Cruz <anthonyomusolo@gmail.com>
2022-01-11 15:37:37 +0300

    Changed contact, slider, team, footer, apps

--- products/apps.py
+++ products/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
-
+from django.contrib import admin
+from .models import Products
 
 
 # To register this category model in our admin
+admin.site.register(Products)
 
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'products'
+    name = 'Products'
