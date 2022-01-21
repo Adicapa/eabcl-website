fe54b4948d82057dc5aa18674ce8e0c61ab4e47a
Cruz <anthonyomusolo@gmail.com>
2022-01-21 12:42:07 +0300

    modified database

--- products/models.py
+++ products/models.py
@@ -10,10 +10,18 @@ class Category(models.Model):
 
 class Products(models.Model):
     name = models.CharField(max_length=300)
+    name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=40)
+    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     category = models.ForeignKey('category', on_delete=models.CASCADE)
     preview_text = models.TextField(max_length=200, verbose_name='Preview Text')
     detail_text = models.TextField(max_length=1000, verbose_name='Detail Text')
+    image = models.ImageField(upload_to='menu/', blank=True)
+    upload = models.FileField(upload_to='menu/')
+    upload = models.FileField(upload_to='menu/')
+    image = models.ImageField(upload_to='menu/', blank=True)
     price = models.FloatField()
     
     
